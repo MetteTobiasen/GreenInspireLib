967737d2e2b99353f54d00ad89597044a6caad7a
MetteTobiasen <tobiasen_1@hotmail.com>
2025-01-02 16:08:22 +0100

    tilføjet ApplicationDbContext klasse til api

---
+++ GreenInspireAPI/AuthenticationData/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace GreenInspireAPI.AuthenticationData
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        {
+        }
+    }
+}
